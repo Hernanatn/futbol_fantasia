d785514edd5a5da0cd06ecc26f99d5a7b0ba801d
Herni <hernanatn@gmail.com>
2024-12-29 22:42:50 -0300

    dist v0.0.1 -alpha

--- compilar.ps1
+++ compilar.ps1
@@ -2,14 +2,15 @@ param(
     [switch]$correr = $false
 )
 
-#Copy-Item -Force -Recurse -Path "compilar/Debug/*" -Destination "distribucion";
 cmake -S . --preset=ff
 cmake --build compilar;
+
+Remove-Item -Force -Recurse "distribucion/*";
 
 Compress-Archive -Path "compilar/Debug/*" -DestinationPath "distribucion/ff.zip"
 iscc instalador/instalador.iss
 
 if($correr){
-Expand-Archive -Path "distribucion/ff.zip" -DestinationPath "distribucion/ff"
-distribucion/ff/futbol_fantasia.exe 
+Expand-Archive -Path "distribucion/ff.zip" -DestinationPath "distribucion/ff";
+distribucion/ff/futbol_fantasia.exe;
 }
--- fuente/futbol_fantasia.cpp
+++ fuente/futbol_fantasia.cpp
@@ -1,13 +1,36 @@
 
 #include "raylib.h"
+#include "raylib.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+#include "raymath.h"
+
+const char * titulo = "fútbol Fantasía";
+const int anchoPantalla = 1600;
+const int altoPantalla = 900;
+
+void Inicializar(){
+    InitAudioDevice();
+};
+void Actualizar(){};
+void Renderizar(){};
+void Cerrar(){};
+
 
 int main(int argc, char** argv){
-    InitWindow(1600, 900, "Fútbol Fantasía");
+    InitWindow(anchoPantalla,altoPantalla, titulo);
+    SetTargetFPS(30);
+
+    Inicializar();
     while (!WindowShouldClose()) {
+        Actualizar();
         BeginDrawing();
         ClearBackground(GREEN);
+        GuiLabel({10,10,300,300}, titulo);
+        Renderizar();
         EndDrawing();
     }
+    Cerrar();
     CloseWindow();
     return 0;
 }
